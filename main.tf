03d29f4e2ac77d5445a72d2f2c6347fa65299ea7
Naoki0722 <mattu.nao722@gmail.com>
2021-09-29 00:46:45 +0900

    add policy & s3 bucket

--- main.tf
+++ main.tf
@@ -1,14 +1,18 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+provider "aws" {
+  region = "ap-northeast-1"
+}
 
-  # tags = {
-  #   "Name" = "example"
-  # }
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
 
-  user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-EOF
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
