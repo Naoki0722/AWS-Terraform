114ce9591c36fddd9a24c1c886996c442de188b6
Naoki0722 <mattu.nao722@gmail.com>
2021-09-30 08:47:04 +0900

    add multi AZ

--- vpc.tf
+++ vpc.tf
@@ -8,12 +8,19 @@ resource "aws_vpc" "example" {
   }
 }
 
-
-resource "aws_subnet" "public" {
+# パブリックサブネット構築
+resource "aws_subnet" "public_0" {
   vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1c"
 }
 
 resource "aws_internet_gateway" "example" {
@@ -30,30 +37,125 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public_0" {
+  subnet_id      = aws_subnet.public_0.id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_subnet" "private" {
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+# プライベートサブネット構築
+resource "aws_subnet" "private_0" {
   vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.64.0/24"
+  cidr_block              = "10.0.65.0/24"
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = false
 }
 
-resource "aws_route_table" "private" {
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.66.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private_0" {
   vpc_id = aws_vpc.example.id
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
 }
 
-resource "aws_eip" "nat_gateway" {
+resource "aws_route" "private_0" {
+  route_table_id         = aws_route_table.private_0.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id         = aws_route_table.private_1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "private_0" {
+  subnet_id      = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_eip" "nat_gateway_0" {
   vpc = true
   depends_on = [
     aws_internet_gateway.example
   ]
+}
+
+resource "aws_eip" "nat_gateway_1" {
+  vpc = true
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id     = aws_subnet.public_0.id
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on = [
+    aws_internet_gateway.example
+  ]
+}
+
+
+# # セキュリティーグループ
+# resource "aws_security_group" "example" {
+#   name   = "example"
+#   vpc_id = aws_vpc.example.id
+# }
+
+# # インバウンド(in側)
+# # HTTPで通信できるようport80を開ける
+# resource "aws_security_group_rule" "ingress_example" {
+#   type              = "ingress"
+#   from_port         = "80"
+#   to_port           = "80"
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.example.id
+# }
+
+
+# # アウトバウンド(out側)
+# resource "aws_security_group_rule" "egress_example" {
+#   type              = "egress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.example.id
+# }
+
+module "example_sg" {
+  source      = "./security_group"
+  name        = "module-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = "80"
+  cidr_blocks = ["0.0.0.0/0"]
+
 }
