0af191bff88a4d9ad1c368ebed6970a170b6399a
Naoki0722 <mattu.nao722@gmail.com>
2021-09-30 08:48:28 +0900

    remove comment

--- vpc.tf
+++ vpc.tf
@@ -122,35 +122,6 @@ resource "aws_nat_gateway" "nat_gateway_1" {
   ]
 }
 
-
-# # セキュリティーグループ
-# resource "aws_security_group" "example" {
-#   name   = "example"
-#   vpc_id = aws_vpc.example.id
-# }
-
-# # インバウンド(in側)
-# # HTTPで通信できるようport80を開ける
-# resource "aws_security_group_rule" "ingress_example" {
-#   type              = "ingress"
-#   from_port         = "80"
-#   to_port           = "80"
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   security_group_id = aws_security_group.example.id
-# }
-
-
-# # アウトバウンド(out側)
-# resource "aws_security_group_rule" "egress_example" {
-#   type              = "egress"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   security_group_id = aws_security_group.example.id
-# }
-
 module "example_sg" {
   source      = "./security_group"
   name        = "module-sg"
